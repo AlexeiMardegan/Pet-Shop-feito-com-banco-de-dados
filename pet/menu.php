a3890aedaa8daafaba1cba552ae1b9f09ce8e475
Alexei Mardegan <76541124+AlexeiMardegan@users.noreply.github.com>
2022-08-01 17:21:56 -0300

    Update menu.php

--- pet/menu.php
+++ pet/menu.php
@@ -2,4 +2,12 @@
 include 'conecta.php';
 $usuario = $_SESSION["user"];
 $menu_query = "SELECT * FROM usuario WHERE login= '$usuario' AND tipo = '1' ";
+$result = $con->query($menu_query);
+if($result->num_rows > 0)
+{
+
+}
+else{
+    
+}
 ?>
